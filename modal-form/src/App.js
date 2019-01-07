ca3cd9f42c2ef4c8bd30ee246d70645756de85a7
weikaipan <weikai.pan1023@gmail.com>
2019-01-07 02:13:58 -0500

    version 2

--- modal-form/src/App.js
+++ modal-form/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import InteractiveGrid from './components/Instrument';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+      justifyContent: 'center'
+  }
+});
 
 class App extends Component {
+
   render() {
+    const { classes } = this.props;
+
     return (
       <div className="App">
-        <InteractiveGrid />
+        <Card classes={classes.root}>
+          <CardHeader title="Instruments" style={{ textAlign: 'left' }}/>
+          <InteractiveGrid />
+        </Card>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- modal-form/src/components/Instrument.js
+++ modal-form/src/components/Instrument.js
@@ -1,65 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Radio from '@material-ui/core/Radio';
-import Paper from '@material-ui/core/Paper';
 import Button from "@material-ui/core/Button";
+import Paper from '@material-ui/core/Paper';
+import Icon from '@material-ui/core/Icon';
+import InstrumentCard from './InstrumentCard';
+
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 import Modal from 'react-modal';
-
+import { Container, Row, Col } from 'reactstrap';
 import { withStyles } from '@material-ui/core/styles';
-import { Form, Text, Select } from 'react-form';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const customStyles = {
-    content : {
-        top                   : '50%',
-        left                  : '50%',
-        right                 : 'auto',
-        bottom                : 'auto',
-        marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)'
-    }
-};
-
-const statusOptions = [
-    {
-      label: 'Single',
-      value: 'single',
-    },
-    {
-      label: 'In a Relationship',
-      value: 'relationship',
-    },
-    {
-      label: "It's Complicated",
-      value: 'complicated',
-    },
-]
-
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  demo: {
-    height: 240,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    height: '100%',
-    color: theme.palette.text.secondary,
-  },
-  control: {
-    padding: theme.spacing.unit * 2,
-  },
-  button: {
-    color: 'red',
-    opacity: '0.5',
-  },
-});
 
 const validate = username => !username || username.trim() === '' ? 'Username is a required field' : null
 
@@ -80,109 +36,219 @@ const asyncValidate = username => new Promise((resolve, reject) =>
     }, 2000)
 )
 
+const currencies = [
+    // {
+    //   value: 'USD',
+    //   label: '$',
+    // },
+    // {
+    //   value: 'EUR',
+    //   label: '€',
+    // },
+    // {
+    //   value: 'BTC',
+    //   label: '฿',
+    // },
+    // {
+    //   value: 'JPY',
+    //   label: '¥',
+    // },
+];
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+    flexGrow: 1,
+  }, 
+  icon: {
+    margin: theme.spacing.unit * 2,
+  },
+  demo: {
+    height: 240,
+  },
+  paper: {
+   height: 140,
+   width: 100
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+  button: {
+    color: '#7CEECE',
+    height: '100%',
+    width: '100%',
+  },
+  addPaper: {
+    backgroundColor: '#7CEECE',
+    opacity: '0.5',
+    height: 140,
+    width: 100
+  },
+  modalSave: {
+    backgroundColor: '#7CEECE',
+    color: 'white',
+    flexGrow: 1,
+  },
+  modalCancel: {
+    color: '#7CEECE',
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  bottomToolBar: {
+    backgroundColor: 'grey',
+  },
+});
+
 class InteractiveGrid extends React.Component {
-
-  constructor() {
-    super();
-
+  constructor (props) {
+    super(props);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
     this.state = {
-      modalIsOpen: false,
-      direction: 'row',
+        modalIsOpen: false,
+        direction: 'row',
         justify: 'center',
         alignItems: 'center',
+        spacing: '16',
     };
-
-    this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
   }
-
+    
   openModal() {
     this.setState({ modalIsOpen: true });
   }
-
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
-  }
-
+  
   closeModal() {
     this.setState({ modalIsOpen: false });
   }
 
   render() {
     const { classes } = this.props;
-    const { alignItems, direction, justify } = this.state;
+    const { alignItems, direction, justify, spacing } = this.state;
 
     return (
-      <Grid container className={classes.root}>
-        <Grid item xs={12}>
-            <Paper>
-                <FormLabel>Instruments</FormLabel>
-                <Grid
-                    container
-                    spacing={16}
-                    className={classes.demo}
-                    alignItems={alignItems}
-                    direction={direction}
-                    justify={justify}
-                >
-                <Grid item>
-                    <Button variant="contained" color="primary">
-                    </Button>
+        <Grid container className={classes.root} spacing={16}>
+            <Grid item xs={12}>
+            <Grid container 
+                  className={classes.demo} 
+                  justify="center" 
+                  alignItems="center" 
+                  direction="row"
+                  spacing={Number(spacing)}>
+                {[0, 1].map(value => (
+                <Grid key={value} item={3}>
+                    <Paper className={classes.paper}>
+                        <InstrumentCard />
+                    </Paper>
                 </Grid>
-
-                <Grid item>
-                    <Button variant="contained" color="primary">
+                ))}
+                <Grid item={3}>
+                    <Paper className={classes.addPaper}>
+                    <Button onClick={this.openModal} className={classes.button}>
+                        <Icon className={classes.icon} fontSize="large">
+                        add
+                        </Icon>
                     </Button>
-                </Grid>
-
-                <Grid item>
-                    <Button onClick={this.openModal} variant="contained" className={classes.button}>
-                    +
-                    </Button>
-                    <div>
-                        <Modal
-                            isOpen={this.state.modalIsOpen}
-                            onAfterOpen={this.afterOpenModal}
-                            onRequestClose={this.closeModal}
-                            style={customStyles}
-                            contentLabel="Example Modal"
-                            >
-                            <h2 ref={subtitle => this.subtitle = subtitle}>Add an Instrument</h2>
-                            <button onClick={this.closeModal}>close</button>
-                        <Form >
-                            {formApi => (
-                            <form onSubmit={formApi.submitForm} id="form6">
-                                <Text
-                                    field="username" id="username"
-                                    validate={validate} asyncValidate={asyncValidate} placeholder={"Name"}
-                                />
-                                <Select field="status" id="select-input-status" options={statusOptions} className="mb-4" />
-                                <Text
-                                    field="description" id="description"
-                                    validate={validate} asyncValidate={asyncValidate} placeholder={"Description"}
-                                />
-                                <Text
-                                    field="cost" id="cost"
-                                    validate={validate} asyncValidate={asyncValidate} placeholder={"Cost"}
-                                />
-                                <Button type="submit" variant="contained" className={classes.button}>
-                                Cancel
-                                </Button>
-                                <Button onClick={this.closeModal} variant="contained" className={classes.button}>
-                                Save
-                                </Button>
-                            </form>
-                            )}
-                        </Form>
-                        </Modal>
-                    </div>
+                    </Paper>
+                    <Modal
+                        isOpen={this.state.modalIsOpen}
+                        onAfterOpen={this.afterOpenModal}
+                        onRequestClose={this.closeModal}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                    >
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            Add an Instrument
+                        </Typography>
+                        <Button onClick={this.closeModal} >
+                            <Icon className={classes.icon} fontSize="large">
+                            close
+                            </Icon>
+                        </Button>
+                    </Toolbar>
+                        <form className={classes.container} noValidate autoComplete="off">
+                            <TextField
+                                id="outlined-name"
+                                label="Name"
+                                className={classes.textField}
+                                value={this.state.name}
+                                // onChange={this.handleChange('name')}
+                                margin="normal"
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <TextField
+                                id="outlined-select-currency-native"
+                                select
+                                label="Type"
+                                className={classes.textField}
+                                value={this.state.currency}
+                                // onChange={this.handleChange('currency')}
+                                SelectProps={{
+                                    native: true,
+                                    MenuProps: {
+                                    className: classes.menu,
+                                    },
+                                }}
+                                placeholder="Type"
+                                fullWidth
+                                margin="normal"
+                                variant="outlined"
+                            >   
+                            {currencies.map(option => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                            ))}
+                            </TextField>
+                            <TextField
+                                id="outlined-type"
+                                label="Description"
+                                className={classes.textField}
+                                value={this.state.type}
+                                // onChange={this.handleChange('name')}
+                                margin="normal"
+                                variant="outlined"
+                                fullWidth
+                                helperText="Up to 300 Characters"
+                            />
+                            <TextField
+                                id="outlined-cost"
+                                label="$ Cost"
+                                className={classes.textField}
+                                value={this.state.cost}
+                                // onChange={this.handleChange('name')}
+                                margin="normal"
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </form>
+                        <Toolbar className={classes.bottomToolBar}>
+                            <Button onClick={this.closeModal} className={classes.modalCancel} >Cancel</Button>
+                            <Button variant="outlined" type="submit" className={classes.modalSave} >Save</Button>
+                        </Toolbar>
+                    </Modal>
                 </Grid>
             </Grid>
-            </Paper>
+            </Grid>
         </Grid>
-      </Grid>
     );
   }
 }
